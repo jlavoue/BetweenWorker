65afb3af2985cc70201b328a0891fe30e8877929
Jérôme Lavoué <jerome.lavoue@umontreal.ca>
2021-10-16 09:35:40 +0200

    refining descriptive analysis

--- scripts/project_NullHypothesis/Descriptive_analysis.R
+++ scripts/project_NullHypothesis/Descriptive_analysis.R
@@ -7,7 +7,17 @@
 #' 
 #' 
 
+#+ r setup, include=FALSE, cache = FALSE
+require("knitr")
+## setting working directory
+opts_knit$set(root.dir = rprojroot::find_rstudio_root_file())
+
+#+ libraries, include = FALSE
+
 library(ggplot2)
+library(here)
+
+#+ Analysis, include=FALSE
 
 data <- read.csv('Raw data/Kromout93Table/kromhout.gsd.database.csv',stringsAsFactors=F)
 
@@ -15,4 +25,11 @@ data$rho <- data$sb^2 / (data$sb^2 + data$sw^2)
 
 #' The ECDF of the 165 within worker correlation values in Kromhout et al 1993
 
+
+#+ Plot, echo=FALSE
+
 ggplot( data , aes(rho) ) + stat_ecdf(geom = "point") + labs(title="Empirical Cumulative Density Function",y = "Cumulative probability", x="Within worker correlation")
+
+
+print(getwd())
+
---
+++ scripts/project_NullHypothesis/Descriptive_analysis.md
@@ -0,0 +1,11 @@
+Descriptive analysis of the Kromhout et al 1993 database
+================
+Jérôme Lavoué
+October 15, 2021
+
+The ECDF of the 165 within worker correlation values in Kromhout et al
+1993
+
+![](Descriptive_analysis_files/figure-gfm/Plot-1.png)<!-- -->
+
+    ## [1] "C:/jerome/github/BetweenWorker"
